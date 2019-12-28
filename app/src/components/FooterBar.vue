4cfd4bcc47dd501f1d6fdc2f7cf739e8fdf50d87
Lin <2973732939@qq.com>
2019-12-28 08:49:27 +0800

    猫眼电影

---
+++ app/src/components/FooterBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="foots">
+    <router-link to="/">电影</router-link>
+    <router-link to="/about">影院</router-link>
+    <router-link to="/mine">我的</router-link>
+  </div>
+</template>
